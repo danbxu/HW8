c92ba3b4725074991f9b76a0168f34229de003f1
danielxu <danielxu@localhost>
2020-12-16 18:47:11 -0800

    Wrote all needed comments

--- HW8/src/Destroyer.java
+++ HW8/src/Destroyer.java
@@ -1,8 +1,10 @@
 
 public class Destroyer extends Ship {
 
-	
-	
+	/**
+	 * Destroyer constructor with length set to 2 and initialize array of hit array 
+	 * depending on the length of the ship. 
+	 */
 	Destroyer() {
 		this.length = 2;
 		this.hit = new boolean[4]; 
@@ -11,17 +13,21 @@ public class Destroyer extends Ship {
 		}
 	}
 	
+	//Overridden to return appropriate length 
 	@Override 
 	public int getLength() {
 		return length;
 	}
 	
+	//To return the ship type
 	@Override
 	String getShipType() {
 		return "Destroyer";
 	}
 	
-	
+	/*
+	 * Checks the ship's hit array for number of trues (max number = ship length)
+	 */
 	@Override 
 	boolean isSunk() {
 		int counter = 0;
@@ -38,6 +44,11 @@ public class Destroyer extends Ship {
 		}
 	}
 	
+	/**
+	 * Overridden to give appropriate strings to show state of the ship
+	 * "x" if the ship is sunk
+	 * "S" if the ship is not sunk but hit at a certain location
+	 */
 	@Override
 	public String toString() {
 		if (this.isSunk()) {
--- HW8/src/Submarine.java
+++ HW8/src/Submarine.java
@@ -1,11 +1,10 @@
 
 public class Submarine extends Ship {
 
-	//TODO constructor 
-	
-	//create an constructor to override
-	// one space
-	
+	/**
+	 * Submarine constructor with length set to 1 and initialize array of hit array 
+	 * depending on the length of the ship. 
+	 */
 	Submarine() {
 		this.length = 1;
 		this.hit = new boolean[4]; 
@@ -14,16 +13,21 @@ public class Submarine extends Ship {
 		}
 	}
 	
+	//Overridden to return appropriate length 
 	@Override 
 	public int getLength() {
 		return length;
 	}
 	
+	//To return the ship type
 	@Override
 	String getShipType() {
 		return "Submarine";
 	}
 	
+	/*
+	 * Checks the ship's hit array for number of trues (max number = ship length)
+	 */
 	@Override 
 	boolean isSunk() {
 		int counter = 0;
@@ -40,6 +44,11 @@ public class Submarine extends Ship {
 		}
 	}
 	
+	/**
+	 * Overridden to give appropriate strings to show state of the ship
+	 * "x" if the ship is sunk
+	 * "S" if the ship is not sunk but hit at a certain location
+	 */
 	@Override
 	public String toString() {
 		if (this.isSunk()) {
