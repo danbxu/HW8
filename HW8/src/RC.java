c60f4676eaacbcd616b081152afd644bc2fea0f7
danielxu <danielxu@localhost>
2020-12-17 15:44:11 -0800

    Update

--- HW8/src/RC.java
+++ HW8/src/RC.java
@@ -1,9 +1,21 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 
+ * @author danielxu and coreyparker
+ *
+ */
 
 public class RC {
 
- static int digit = 0;
-
+	static int digit = 0;
+	/**
+	 * killComma method iterates to through each char at each index and checks if it is a comma
+	 * @param string
+	 * @return
+	 */
 	public static String killCommas(String string) {
 		//Base case: no string
 		if (string == "") throw new IllegalArgumentException ("No string");
@@ -25,7 +37,7 @@ public class RC {
 			return string.charAt(0) + killCommas(string.substring(1));
 		}
 	}
-	
+
 
 	public static int sumDigits(int num) {
 		//Base case: number is zero
@@ -42,42 +54,57 @@ public class RC {
 			return sumDigits(num / 10);
 		}
 	}
-	
-	
-	public void powerSet(int[] arr) {
 
-		//Base case: empty array
-		if (arr.length == 0) {
-			System.out.println("[]");
+
+	public void helper(int[] arr, List<Integer> current, List<List<Integer>> powerSet, int start) {
+
+		//BC
+		//This will add from current and create different permutation into ArrayList<Integer>
+		powerSet.add(new ArrayList<Integer> (current));
+
+		//Other cases
+		for (int i = start; i < arr.length; i++) {
+			current.add(arr[i]);
+			helper(arr, current, powerSet, i + 1); //arr, current, and powerset stay the same, and i + 1 moves to the next
+			current.remove(current.size() - 1);
 		}
-		//Base case: array with 1 element
-		if (arr.length == 1) {
-			System.out.println("{" + arr + "}");
+
+	}
+
+
+
+
+	public void powerSet(int[] arr) {
+		ArrayList<Integer> current = new ArrayList<>();
+		List<List<Integer>> powerSet = new ArrayList<>();
+		int start = 0;
+
+		this.helper(arr, current, powerSet, start);
+
+		for (int i = 0; i < powerSet.size(); i++) {
+			System.out.println(powerSet.get(i));
 		}
-		//Cases where array size is greater than 1
-		if (arr.length > 1) {
-			
-		}
-		
+
 	}
 
 	public static void main(String[] args) {
-//		System.out.println(killCommas("a,c"));
-//		System.out.println(killCommas(",abc,d"));
-//		System.out.println(killCommas(",___,"));
-//		System.out.println(killCommas("lma,o,"));
-//		System.out.println(killCommas(",,"));
-//		System.out.println(killCommas("a,c,d,,,"));
-//		System.out.println(killCommas(""));
-		
-//		System.out.println(sumDigits(111)); // Need to ask about if i need to clear the variables
-//		System.out.println(sumDigits(123));
-		
-		
-		int[] arr = new int[] {1};
-		System.out.println(Arrays.toString(arr));
-		
-	
+		//		System.out.println(killCommas("a,c"));
+		//		System.out.println(killCommas(",abc,d"));
+		//		System.out.println(killCommas(",___,"));
+		//		System.out.println(killCommas("lma,o,"));
+		//		System.out.println(killCommas(",,"));
+		//		System.out.println(killCommas("a,c,d,,,"));
+		//		System.out.println(killCommas(""));
+
+		//		System.out.println(sumDigits(111)); // Need to ask about if i need to clear the variables
+		//		System.out.println(sumDigits(123));
+
+
+		RC a = new RC();
+		int[] arr = {1,2,3};
+		a.powerSet(arr);
+
+
 
 	}
 }
